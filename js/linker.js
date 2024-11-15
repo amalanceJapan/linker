cbdc85fbe1029625441dda0dcab34db3dc51c428
masahide <yasukawa@amalance.jp>
2024-11-15 09:11:42 +0900

    a

--- js/linker.js
+++ js/linker.js
@@ -5,45 +5,51 @@ document.addEventListener("DOMContentLoaded", function () {
   // 各ボタンにイベントリスナーを追加
   buttons.forEach(button => {
     button.addEventListener("click", function () {
-      // オンラインかどうかを確認
-      if (!navigator.onLine) {
-        // オフラインの場合にアラートを表示
-        alert("インターネットに接続されていません。接続を確認してください。");
-        return; // リダイレクト処理を停止
-      }
+      // インターネット接続確認のために webauth.wifiservice.jp にアクセス
+      fetch("https://webauth.wifiservice.jp", { method: 'HEAD', mode: 'no-cors' })
+        .then(() => {
+          // 接続が確認できた場合のみリダイレクト処理を続行
+          const clickParam = button.getAttribute("data-click");
 
-      // ボタンの `data-click` 属性からパラメータ値を取得
-      const clickParam = button.getAttribute("data-click");
+          var currentUrl = window.location.href;
+          var newFqdn = "webauth.wifiservice.jp";
+          var newUrl = new URL(currentUrl);
+          var urlParams = new URLSearchParams(newUrl.search);
 
-      var currentUrl = window.location.href;
-      var newFqdn = "webauth.wifiservice.jp";
-      var newUrl = new URL(currentUrl);
-      var urlParams = new URLSearchParams(newUrl.search);
+          // `click` パラメータにボタン固有の値を設定
+          urlParams.set('click', clickParam);
 
-      // `click` パラメータにボタン固有の値を設定
-      urlParams.set('click', clickParam);
+          // Force HTTPS if the protocol is HTTP
+          if (newUrl.protocol === "http:") {
+            newUrl.protocol = "https:";
+          }
 
-      // Force HTTPS if the protocol is HTTP
-      if (newUrl.protocol === "http:") {
-        newUrl.protocol = "https:";
-      }
+          // Remove any port number
+          newUrl.port = "";
 
-      // Remove any port number
-      newUrl.port = "";
+          // Set the new hostname (FQDN)
+          newUrl.hostname = newFqdn;
 
-      // Set the new hostname (FQDN)
-      newUrl.hostname = newFqdn;
+          // Add the "/cnctor" subdirectory to the pathname if it doesn't already exist
+          if (!newUrl.pathname.includes("/cnctor")) {
+            newUrl.pathname = "/cnctor" + newUrl.pathname;
+          }
 
-      // Add the "/cnctor" subdirectory to the pathname if it doesn't already exist
-      if (!newUrl.pathname.includes("/cnctor")) {
-        newUrl.pathname = "/cnctor" + newUrl.pathname;
-      }
+          // If 'lang' parameter is not present, add lang=ja
+          if (!urlParams.has('lang')) {
+            urlParams.set('lang', 'ja');
+          }
 
-      // Apply the modified query parameters back to the URL
-      newUrl.search = urlParams.toString();
+          // Apply the modified query parameters back to the URL
+          newUrl.search = urlParams.toString();
 
-      // インターネット接続が確認できた場合のみリダイレクト
-      window.location.href = newUrl.href;
+          // インターネット接続が確認できた場合のみリダイレクト
+          window.location.href = newUrl.href;
+        })
+        .catch(() => {
+          // 接続エラーの場合にアラートを表示
+          alert("インターネットに接続されていないため、指定のURLにアクセスできません。接続を確認してください。");
+        });
     });
   });
 });
